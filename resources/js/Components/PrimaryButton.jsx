e66504842cb8546baa572a38bfa1704e1d3f3cdf
shluf <luthfisalis09@gmail.com>
2024-10-12 18:56:03 +0700

    Update auth page

--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -1,6 +1,7 @@
 export default function PrimaryButton({
     className = '',
     disabled,
+    color,
     children,
     ...props
 }) {
@@ -8,7 +9,9 @@ export default function PrimaryButton({
         <button
             {...props}
             className={
-                `inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 ${
+                `inline-flex items-center rounded-md border border-transparent px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900
+                 ${ color === 'green' ? 'bg-green hover:bg-green-2' : color === 'yellow' ? 'bg-yellow hover:bg-yellow-2' : 'bg-gray-800 hover:bg-gray-700'} 
+                 text-white ring-1 ring-transparent hover:text-white ${
                     disabled && 'opacity-25'
                 } ` + className
             }
--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,18 +1,108 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+// import ApplicationLogo from "@/Components/ApplicationLogo";
+import { Link } from "@inertiajs/react";
 
-export default function Guest({ children }) {
+export default function Guest({ children, button, title }) {
+
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
-                </Link>
+        <section className="relative bg-gray-100 overflow-hidden">
+            <div className="absolute inset-0 flex justify-center z-0">
+                <img
+                    className="object-cover w-full h-full"
+                    src="/img/bg_land_overlay.png"
+                    alt="overlay"
+                />
             </div>
 
-            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
-                {children}
+            <nav className="fixed top-0 left-0 right-0 z-50 transition-all duration-300 ease-in-out bg-white bg-opacity-50 backdrop-blur shadow-md mt-4 mx-4 rounded-lg">
+                <div className="container mx-auto px-4 py-2 flex justify-between items-center">
+                    <a href="#" className="text-2xl font-bold text-gray-800">
+                        <img
+                            src="/logo.svg"
+                            alt="Logo"
+                            className="h-8 inline-block mr-2"
+                        />
+                    </a>
+                    <div className="hidden md:block">
+                        <a href="/#" className="text-gray-800 hover:text-green">
+                            Beranda
+                        </a>
+                        <span className="mx-4 text-green">⋮</span>
+                        <a
+                            href="/#tentang"
+                            className="text-gray-800 hover:text-green"
+                        >
+                            Tentang
+                        </a>
+                        <span className="mx-4 text-green">⋮</span>
+                        <a
+                            href="/#cara-kerja"
+                            className="text-gray-800 hover:text-green"
+                        >
+                            Cara Kerja
+                        </a>
+                        <span className="mx-4 text-green">⋮</span>
+                        <a
+                            href="/#kritik-saran"
+                            className="text-gray-800 hover:text-green"
+                        >
+                            Kritik Saran
+                        </a>
+                        <span className="mx-4 text-green">⋮</span>
+                        <a href="#" className="text-gray-800 hover:text-green">
+                            Bantuan
+                        </a>
+                    </div>
+                    <div className="flex">
+                        {button == "all" ? (
+                            <>
+                                <Link
+                                    href={route("register")}
+                                    className="rounded-md px-3 py-2 bg-yellow hover:bg-yellow-2 text-white font-bold mr-2 ring-1 ring-transparent transition hover:text-white focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                >
+                                    Registrasi
+                                </Link>
+                                <Link
+                                    href={route("login")}
+                                    className="rounded-md px-3 py-2 bg-green hover:bg-green-2 text-white font-bold ring-1 ring-transparent transition hover:text-white focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                >
+                                    Masuk
+                                </Link>
+                            </>
+                        ) : button == "register" ? (
+                            <>
+                                <Link
+                                    href={route("register")}
+                                    className="rounded-md px-3 py-2 bg-yellow hover:bg-yellow-2 text-white font-bold mr-2 ring-1 ring-transparent transition hover:text-white focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                >
+                                    Registrasi
+                                </Link>
+                            </>
+                        ) : (
+                            <Link
+                                href={route("login")}
+                                className="rounded-md px-3 py-2 bg-green hover:bg-green-2 text-white font-bold ring-1 ring-transparent transition hover:text-white focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                            >
+                                Masuk
+                            </Link>
+                        )}
+                    </div>
+                </div>
+            </nav>
+
+            <div className="flex relative z-10 min-h-screen flex-col items-center pt-6 sm:justify-center sm:pt-0">
+                {/* <div>
+                    <Link href="/">
+                        <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
+                    </Link>
+                </div> */}
+                <div>
+                    <h1 className="font-bold text-4xl">{title}</h1>
+                </div>
+
+                <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+                    {children}
+                </div>
             </div>
-        </div>
+        </section>
     );
 }
